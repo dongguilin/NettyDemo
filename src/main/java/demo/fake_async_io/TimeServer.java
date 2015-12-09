ba0aac0056a68a7b832471b50d543a3809520ff4
dongguilin <guilin1_happy@163.com>
2015-12-09 23:00:17 +0800

    伪异步IO

---
+++ src/main/java/demo/fake_async_io/TimeServer.java
@@ -0,0 +1,45 @@
+package demo.fake_async_io;
+
+import demo.bio.TimeServerHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by hadoop on 2015/12/9.
+ * 伪异步I/O的TimeServer
+ */
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port:" + port);
+            Socket socket = null;
+            TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);//创建I/O任务线程池
+            while (true) {
+                socket = server.accept();
+                singleExecutor.execute(new TimeServerHandler(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                System.out.println("The time server close");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
