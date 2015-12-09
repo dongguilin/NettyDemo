a1281fc0f88ad992e162ba5fcf9ae43a0f670a9e
dongguilin <guilin1_happy@163.com>
2015-12-09 22:38:24 +0800

    java bio

---
+++ src/main/java/demo/bio/TimeClient.java
@@ -0,0 +1,61 @@
+package demo.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by hadoop on 2015/12/9.
+ * 同步阻塞I/O的TimeClient
+ */
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is:" + resp);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            socket = null;
+        }
+    }
+}
