afce6a10a03c30b1b4cf481c496e080bb6a93a40
dongguilin <guilin1_happy@163.com>
2015-12-10 22:48:30 +0800

    java NIO

---
+++ src/main/java/demo/nio/TimeClient.java
@@ -0,0 +1,21 @@
+package demo.nio;
+
+/**
+ * Created by hadoop on 2015/12/10.
+ * NIO时间服务器TimeClient
+ */
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        new Thread(new TimeClientHandler("127.0.0.1", port), "TimeClient-001").start();
+    }
+}
---
+++ src/main/java/demo/nio/TimeServer.java
@@ -0,0 +1,22 @@
+package demo.nio;
+
+/**
+ * Created by hadoop on 2015/12/10.
+ * NIO时间服务器TimeServer
+ */
+public class TimeServer {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
